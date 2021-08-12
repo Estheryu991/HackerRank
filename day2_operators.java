77a79cb5c127074803cada75ed546dc8a48f5b3c
Esther YU <togom11@g.skku.edu>
2021-08-12 10:13:41 +0900

    Create day2_operators.java

---
+++ day2_operators.java
@@ -0,0 +1,112 @@
+/*
+int meal_cost: the cost of food before tip and tax
+int tip_percent: the tip percentage
+int tax_percent: the tax percentage
+
+Returns The function returns nothing. Print the calculated value, rounded to the nearest integer.
+*/
+
+public class Car {
+    int maxSpeed
+    int minSpeed = 0;
+  
+  double weight 4079;
+  
+  boolean isTheCarOn = false;
+  char condition = 'A';
+  String nameOfCar = "Lucy";
+  
+  double maxFuel = 16;
+  double currentFuel = 8;
+  
+  int numberOfPeopleInCar = 1;
+  
+  // f(x) = x + 1;
+  // Say x = 5;
+  // f(5) 
+  
+  
+  public Car(int customMaxSpeed, double customWeight, boolean customIsTheCarOn)
+    maxSpeed = customMaxSpeed;
+    weight = customWeight;
+    isTheCarOn = customIsTheCarOn;
+  
+  public void printVariables() {
+    System.out.println("This is the maxSpeed")
+      System.out.println(minSpeed);
+   System.out.println(weight);
+      System.out.println(isTheCarOn);
+      System.out.println(condition);
+      System.out.println(nameOfCar);
+    public void upgradeMinSpeed() {
+    minSpeed = maxSpeed;
+      maxSpeed = maxSpeed
+    }
+    
+  public coid upgradeMinSpeed() {
+    minSpeed = maxSpeed;
+    maxSpeed - maxSpeed + 1;
+  }
+    
+    public void getIn() {
+      // numofPoepleInCar = mumofpplincar +1;
+      numberOfPeopleInCar++;
+    }
+    
+    
+    public static void main(String[] args) {
+  //TODO code application
+      public double howManyMilesTillOutOfGas() {
+        return currenFuel * mpg; 
+      }
+      
+      public double maxMilesPerFillUp() {
+        return maxFuel * mpg;
+      }
+      
+      Car birthdayPresent = new Car(500,5000.545, true);
+      birthdayPresent = new Car(500, 5000.545, true);
+      birthdayPresent.printVariables();
+      Car christmasPresent = new Car(550, 2000, false);
+      christmasPresent.printVariables();
+    }
+    
+    public void getOut() {
+      // numofPoepleInCar = mumofpplincar -1;
+      numberOfPeopleInCar--;
+    }
+    //Custom Car VS Dafault Car
+    //default 
+    
+    public double howManyMilesTillOutOfGas() {
+      return currentFue; * mpg;
+    }
+    
+    public double maxMiles PerFillUp () {
+    return maxFuel * mpg;
+    }
+    
+    public static void main (String[] args) {
+    //todo code application logic here 
+      Car birthdayPresent = new Car(500, 5000.545, true);
+      System.out.println("Birthday Car v1");
+    }
+    public static void printVariables
+    
+    public Car(int custonMinSpeed) {
+//     customMinSpeed = minSpeed;
+        minSpeed = customMinSpeed;
+  }
+    
+    tools -> methods -> install
+    
+      declare
+      allocate
+      initialize
+    
+//     parameter: a variable that has input to data. 
+//       argument: a piece of data passed through a function whose value is the value of the parameter
+    
+    
+      
+      // MATH
